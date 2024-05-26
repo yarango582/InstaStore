0e90c0e33c78df543e4ded9f78109732cd538889
yarangodev <yeisonarango03@gmail.com>
2024-05-26 17:18:41 -0500

    feat: add documentation swagger

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import storeRoutes from './routes/store.route';
 import logRoutes from './routes/log.route';
 import connectDB from './config/database.config';
 import logger from './utils/logger/logger.util';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../swagger.json';
 
 dotenv.config();
 
@@ -15,6 +17,7 @@ connectDB();
 
 app.use('/api/v1/stores', storeRoutes);
 app.use('/api/v1/logs', logRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
